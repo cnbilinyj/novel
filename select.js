fd9b307999fcc0eb1b30fc456aa8a9b6c3f4db51
王嘉睿 <99505726+cnbilinyj@users.noreply.github.com>
2022-08-01 22:48:29 +0800

    Update select.js

--- select.js
+++ select.js
@@ -12,7 +12,16 @@ function down(blank,ct){
 var down=confirm("请选择您要跳转到的链接\n当您点击确认后，将跳转到空翼云储存分享链接处\n当您点击取消时，将跳转到城通网盘分享链接处\n城通网盘的通用访问密码：hhhnyj");
 if (down==true){
 window.open(blank,"newwindow");
-	}else{
+}else{
 window.open(ct,"newwindow");
-	}
+}
+}
+
+function characterSet(novelN){
+var cs = confirm("您的浏览器是否支持utf-8编码？");
+if(cs == true){
+window.open("./" + novelN + "/" + novelN + "0","newwindow");
+}else{
+window.open("./" + novelN + "/" + novelN + "1","newwindow");
+}
 }
